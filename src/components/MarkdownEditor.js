deac997944efb723183e34fdcec7e8812bce941f
AlexanMihai <boleaalexandru20@stud.ase.ro>
2023-01-18 16:44:18 +0200

    Added Save and Cancel Buttons

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -4,6 +4,7 @@ import "./MarkdownEditor.css"
 export function MarkdownEditor() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [showEditor, setShowEditor] = useState(true);
 
   function handleTitleChange(e) {
     setTitle(e.target.value);
@@ -13,8 +14,35 @@ export function MarkdownEditor() {
     setContent(e.target.value);
   }
 
+  function handleCancel() {
+    setShowEditor(false);
+  }
+
+  function handleSave() {
+    if (title && content) {
+      // make API call to save title and content
+      const data = {
+        title: title,
+        content: content
+      };
+      fetch('/api/save', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    } else {
+      // handle error, for example show a message to the user that the fields are required
+      alert('Title and content are required');
+    }
+  }
+
   return (
-    <div className="markdown-editor">
+    <div>
+    {!showEditor && <button onClick={() => setShowEditor(true)}>Open Markdown Editor</button>}
+      {showEditor &&
+      <div className="markdown-editor">
       <h1>
         <input 
           className="markdown-title"
@@ -30,6 +58,9 @@ export function MarkdownEditor() {
         onChange={handleContentChange}
         placeholder="Content"
       />
+      <button className="cancel-button" onClick={handleCancel}>Cancel</button>
+      <button className="save-button" onClick={handleSave}>Save</button>
+    </div>}
     </div>
   );
 }
